1181d220220c5b1fe5170040e12769f544c3925e
suyeon <suyeon_2s@icloud.com>
2022-04-01 16:35:57 +0900

    Initial Commit

---
+++ memoApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  memoApp
+//
+//  Created by 김수연 on 2022/04/01.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ memoApp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  memoApp
+//
+//  Created by 김수연 on 2022/04/01.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
