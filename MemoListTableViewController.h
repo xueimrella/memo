aa73a40e535a06c1e250ed620f2460428a71fa09
suyeon <suyeon_2s@icloud.com>
2022-04-01 17:30:12 +0900

    memoListTableViewController

---
+++ MemoListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MemoListTableViewController.h
+//  memoApp
+//
+//  Created by 김수연 on 2022/04/01.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MemoListTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
